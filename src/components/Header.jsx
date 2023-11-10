f5b31fe9bfdbaab481c50cf258ccb6b2b4a16f09
luidimso <luidi_mso@outlook.com>
2023-11-10 11:44:00 -0300

    Applyig inline style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: "green"
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
